e81596b95227f02410a0acf4a0783406955804fe
Sadula Rushidhar <115465446+S-Rushidhar-1-1999@users.noreply.github.com>
2022-12-27 11:53:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 echo "Cloning Repository"
-git clone https://github.com/Greymattersbot/md-renamebot /md-renamebot
+git clone https://github.com/S-Rushidhar-1-1999/RENAME-BOT /md-renamebot
 cd /md-renamebot 
 echo "installing requirements"
 pip3 install -U -r requirements.txt
